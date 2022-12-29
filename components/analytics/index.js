948b3cb49f122d5a5abed2e3853f543f1ea61cdb
jacy hoag <jacylh@gmail.com>
2022-12-28 23:35:49 -0500

    added vercel analytics stream

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -4,19 +4,21 @@ import SimpleAnalytics from './SimpleAnalytics'
 import Umami from './Umami'
 import Posthog from './Posthog'
 import siteMetadata from '@/data/siteMetadata'
+import { Analytics } from '@vercel/analytics/react'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const Analytics = () => {
+const Analytic = () => {
   return (
     <>
       {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+      {isProduction && <Analytics />}
       {isProduction && siteMetadata.analytics.posthogAnalyticsId && <Posthog />}
     </>
   )
 }
 
-export default Analytics
+export default Analytic
