e80ab2ea19816ff5a7bda47c9eb76344cd12b6ef
jacy hoag <jacylh@gmail.com>
2023-09-17 17:25:54 -0400

    removed Google Analytics

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -1,24 +1,9 @@
-import GA from './GoogleAnalytics'
-import Plausible from './Plausible'
-import SimpleAnalytics from './SimpleAnalytics'
-import Umami from './Umami'
-import Posthog from './Posthog'
-import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from '@vercel/analytics/react'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytic = () => {
-  return (
-    <>
-      {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
-      {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
-      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
-      {isProduction && <Analytics />}
-      {isProduction && siteMetadata.analytics.posthogAnalyticsId && <Posthog />}
-    </>
-  )
+  return <>{isProduction && <Analytics />}</>
 }
 
 export default Analytic
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
   {
-    title: 'Turbine: Squadron Manpower Tracking Suite',
+    title: 'Turbin: Squadron Manpower Tracking Suite',
     description: `Do you print out BLSDM files and pour over them manually to determine what billet to assign incoming members? 
     What if you had a a complete view of your squadrons billet allocation and auto assign based on a few criteria you determine?
      What if you could track everything from annual to upgrade skill-level training, annual requirements, awards and decs, and everything in between. 
